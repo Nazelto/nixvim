5f731dd47dfcc12ad5508a6d8b25494ff6743cee
Nazelto.WSL <1500465005@qq.com>
2026-01-07 15:56:18 +0800

    fix:neovide config

--- config/default.nix
+++ config/default.nix
@@ -63,7 +63,7 @@ _: {
     if vim.g.neovide then
         -- 透明度
         vim.g.neovide_opacity = 0.7
-        vim.g.neovide_normal_opacity = 0.75
+        vim.g.neovide_normal_opacity = 0.80
         -- vim.g.neovide_transparency = 0.7
         
         -- 字体
